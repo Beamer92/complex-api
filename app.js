9a386370524d428c67be4423937f68e60e51fa27
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-13 15:21:46 -0800

    Trying again to update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((err, req, res, next) => {
   })
   
   app.use((req, res, next) => {
-    res.status(404).json({ error: { message: 'You have magnificently managed to find absolutely nothing' }})
+    res.status(404).json({ error: { message: 'You have magnificently managed to find abolutely nothing' }})
   })
 
 if (process.env.NODE_ENV !== 'development') {
