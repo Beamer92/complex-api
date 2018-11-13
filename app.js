be6804687fb2ebf604f4c9c203c9137b7e77512e
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-13 15:20:17 -0800

    changing default error message

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((err, req, res, next) => {
   })
   
   app.use((req, res, next) => {
-    res.status(404).json({ error: { message: 'Not found' }})
+    res.status(404).json({ error: { message: 'You have magnificently managed to find absolutely nothing' }})
   })
 
 if (process.env.NODE_ENV !== 'development') {
